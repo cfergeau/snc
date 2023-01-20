5567231a9b2a085d7349dc862c952b69846f7928
Christophe Fergeau <cfergeau@redhat.com>
2023-01-20 13:19:52 +0100

    initial centos-stream9 support
    2 options:
    - use generic cloud image + cloud-init - 'cloud-user', but can specify
      ssh keys
    - use ISO + virt-install --unattended, but adding a ssh key would be
      done after install

--- snc.sh
+++ snc.sh
@@ -25,37 +25,57 @@ CRC_INSTALL_DIR=crc-tmp-install-data
 rm -fr ${CRC_INSTALL_DIR}
 mkdir ${CRC_INSTALL_DIR}
 chcon --verbose unconfined_u:object_r:svirt_home_t:s0 ${CRC_INSTALL_DIR}
-cp fcos-config.yaml ${CRC_INSTALL_DIR}
 
 # Generate a new ssh keypair for this cluster
 # Create a 521bit ECDSA Key
 rm id_ecdsa_crc* || true
 ssh-keygen -t ecdsa -b 521 -N "" -f id_ecdsa_crc -C "core"
 
-${YQ} eval --inplace ".passwd.users[0].ssh_authorized_keys[0] = \"$(cat id_ecdsa_crc.pub)\"" ${CRC_INSTALL_DIR}/fcos-config.yaml
-
-# Create the ign config 
-${PODMAN} run -i --rm quay.io/coreos/butane:release --pretty --strict < ${CRC_INSTALL_DIR}/fcos-config.yaml > ${CRC_INSTALL_DIR}/fcos-config.ign
-
-# Validate ign config
-${PODMAN} run --pull=always --rm -i quay.io/coreos/ignition-validate:release - < ${CRC_INSTALL_DIR}/fcos-config.ign
-
 # Download the latest fedora coreos latest qcow2
 mkdir ${PWD}/${CRC_INSTALL_DIR}/tmp/
-${PODMAN} run --pull=always --rm -v ${PWD}/${CRC_INSTALL_DIR}/tmp/:/data:Z -w /data quay.io/coreos/coreos-installer:release download -a ${ARCH} -s stable -p qemu -f qcow2.xz --decompress
-sudo mv ${CRC_INSTALL_DIR}/tmp/fedora-coreos-*-qemu.${ARCH}.qcow2 /var/lib/libvirt/images/fedora-coreos-qemu.${ARCH}.qcow2
+# Download HTML directory listing of cloud images sorted in date descending order,
+# only keep the lines corresponding to an actual image (".*GenericCloud.*qcow2"),
+# and extract the filename from '<a href=".*qcow2">'
+MIRROR="https://cloud.centos.org/centos/9-stream/${ARCH}/"
+latest_image=$((curl --silent "${MIRROR}/images/?C=M;O=D"; true) | grep --max-count=1 GenericCloud.*qcow2\" | sed 's/.*<a href="\(.*.qcow2\)".*>/\1/')
+echo "${latest_image}"
+if [ ! -e ${latest_image} ]; then
+	curl -L -O "${MIRROR}/images/${latest_image}"
+fi
+sudo mv ${CRC_INSTALL_DIR}/tmp/CentOS-Stream-GenericCloud-9-*.${ARCH}.qcow2 /var/lib/libvirt/images/CentOS-Stream-GenericCloud-9.${ARCH}.qcow2
 rmdir ${PWD}/${CRC_INSTALL_DIR}/tmp/
 
-sudo setfacl -m u:qemu:rx $HOME
-sudo systemctl restart libvirtd
+#sudo setfacl -m u:qemu:rx $HOME
+#sudo systemctl restart libvirtd
 
 create_json_description
 
-# Start the VM using virt-install command
+#CAREFUL: different --cloud-init options between rhel8 and f37
+# -> see c8afd1f5, ssh-key is aliased to root-ssh-key
+# rhel8:
+#$ virt-install --cloud-init=?
+#--cloud-init options:
+#  disable
+#  meta-data
+#  root-password-file
+#  root-password-generate
+#  ssh-key
+#  user-data
+#
+# f37:
+# --cloud-init options:
+#  clouduser-ssh-key
+#  disable
+#  meta-data
+#  network-config
+#  root-password-file
+#  root-password-generate
+#  root-ssh-key
+#  user-data
 sudo ${VIRT_INSTALL} --name=${CRC_VM_NAME} --vcpus=2 --ram=2048 --arch=${ARCH}\
-	--import --graphics=none \
-	--qemu-commandline="-fw_cfg name=opt/com.coreos/config,file=${PWD}/${CRC_INSTALL_DIR}/fcos-config.ign" \
-	--disk=size=31,backing_store=/var/lib/libvirt/images/fedora-coreos-qemu.${ARCH}.qcow2 \
-	--os-variant=fedora-coreos-stable \
+        --import --graphics=none \
+	--cloud-init ssh-key=$(pwd)/id_ecdsa_crc.pub,disable=on \
+	--disk=size=31,backing_store=/var/lib/libvirt/images/CentOS-Stream-GenericCloud-9.${ARCH}.qcow2 \
+        --os-variant=centos-stream9 \
 	--noautoconsole --quiet
 sleep 120
